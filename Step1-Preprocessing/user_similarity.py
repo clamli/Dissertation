ccfebcc1790dac43aeac9f8385677dc99b2f1e68
clamli <boyizjuer2017@gmail.com>
2018-05-23 11:44:05 +0800

    modify the way to calculate user similarity

--- Step1-Preprocessing/user_similarity.py
+++ Step1-Preprocessing/user_similarity.py
@@ -23,29 +23,26 @@ if (__name__ == '__main__'):
 
 	# generate user similarity
 	rating_matrix_train = (rating_matrix_train - np.sum(rating_matrix_train, axis=0) / np.sum(rating_matrix_train != 0, axis=0)) * (rating_matrix_train!=0)
+	rating_matrix_train_2 = rating_matrix_train**2
+	# user_similarity_matrix = np.dot(rating_matrix_train.T, rating_matrix_train) / (np.dot(rating_matrix_train_2.T, rating_matrix_train_2)**0.5 + 1e-9)
 	row_num = rating_matrix_train.shape[0]
 	col_num = rating_matrix_train.shape[1]
 	user_similarity_matrix = np.zeros((col_num, col_num))
-	denominatorM = np.sum(rating_matrix_train**2, axis=0)
-	print("denominator done!")
-	flag = (rating_matrix_train != 0)
-	print("flag done!")
 	nominatorM = np.dot(rating_matrix_train.T, rating_matrix_train)
 	print("nominator done!")
-	rating_matrix_train_2 = rating_matrix_train**2
-	print("rating_matrix_train ^2 done!")
 	cnt = 0
 	for i in range(col_num):
 		cnt += 1
 		print("progress: %d / %d"%(cnt, col_num), end="\r")
-		denominator1 = denominatorM[i]
-		denominator2 = np.sum((flag[:, i].reshape(row_num, 1))*rating_matrix_train_2, axis = 0)
-		user_similarity_matrix[i] = nominatorM[i] / (denominator1**0.5 * denominator2**0.5 + 1e-5)
+		flag = ((rating_matrix_train[:, i]!=0).reshape(row_num, 1))*(rating_matrix_train!=0)
+		user_similarity_matrix[i] = nominatorM[i] / ((np.dot(rating_matrix_train_2[:, i].T, flag)**0.5) * (np.sum(rating_matrix_train_2*flag, axis=0)**0.5) + 1e-9)
 	# or it will be 0 for some users
-	np.fill_diagonal(user_similarity_matrix, 1)
+	# np.fill_diagonal(user_similarity_matrix, 1)
 	print("\ndone!")
 
 	# transfer to dataframe and save to file
+	# rw.write2file(user_similarity_matrix, "Data/test")
 	df_user_similarity_matrix = pd.DataFrame(user_similarity_matrix,index=list(uid.keys()),columns=list(uid.keys()))
+	del user_similarity_matrix
 	rw.write2file(df_user_similarity_matrix, foutput_user_similarity)
 	print("file saved done!")
